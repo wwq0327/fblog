61d6671828aa0c30254d1638b79458853f4a2e5d
wwq0327 <wwq0327@gmail.com>
2011-10-22 10:31:50 +0800

    fix

--- fblog/views/account.py
+++ fblog/views/account.py
@@ -1,9 +1,9 @@
 #coding:utf-8
 
-from flask import Module, request, render_template, redirect, url_for, flash, Blueprint
+from flask import request, render_template, redirect, url_for, flash, Blueprint
 
-from flaskext.login import (LoginManager, current_user, login_required, login_user,
-                          logout_user, UserMixin, AnonymousUser, confirm_login,
+from flaskext.login import (current_user, login_required, login_user,
+                          logout_user, confirm_login,
                           fresh_login_required)
 
 from fblog.extensions import login_manager
@@ -12,14 +12,7 @@ from fblog.models import Anonymous, User, LoginUser
 from fblog.forms import LoginForm
 from fblog.views import admin
 
-## account = Module(__name__)
 account = Blueprint("account", __name__)
-
-## login_manager = LoginManager()
-## login_manager.anonymous_user = Anonymous
-## login_manager.login_view = 'account.login'
-## login_manager.login_message = u'你需要登录后才能进行下一步操作'
-## login_manager.refresh_view = 'fblog.views.account.reauth'
 
 @login_manager.user_loader
 def load_user(id):
@@ -50,15 +43,6 @@ def login():
                 flash(u"用户名不存在，或与密码不匹配")
 
     return render_template("admin/login.html", form=form)
-
-## @account.route('/reauth', methods=['GET', 'POST'])
-## @login_required
-## def reauth():
-##     if request.method == 'POST':
-##         confirm_login()
-##         flash(u"用户认证")
-##         return redirect(request.args.get('next') or url_for('blog.index'))
-##     return render_template('admin/reauth.html')
 
 @account.route('/logout')
 @login_required
